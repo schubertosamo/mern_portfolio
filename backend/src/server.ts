0ac6321242d69566d1b9fb7ce9adef3115c63e28
Shu Matsunaga <107782526+schubertosamo@users.noreply.github.com>
2023-03-17 23:11:04 +0900

    various modification

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
-import express from "express";
-const app =  express();
-const port = 5000;
+import app from "./app";
+import env from "./util/validateEnv"
+import mongoose from "mongoose";
 
+const port = env.PORT;
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
-app.listen(port, () => {
-    console.log("Server running on port: "+ port );
-});
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+.then(() => {
+    console.log('Mongoose connected')
+    app.listen(port, () => {
+        console.log("Server running on port: "+ port );
+    });
+})
+.catch(console.error)
